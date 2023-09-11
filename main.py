6ea69f7a15abc0068324c0529b3c6087b6ae2ef0
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2023-09-11 22:48:42 +0300

    setting_ship_coords function is created
    this function is called,when we meet with "1" in matrix field
    it returns list of tupples with ship coordinates

--- main.py
+++ main.py
@@ -14,20 +14,32 @@ def validate_battlefield(field: list):
     added_coordinates = set()
     row_size = len(field[0])
     column_size = len(field)
+    is_1 = lambda x: x == "1"
 
-    def is_other_ship_around(ship_coords: list, row_size: int, column_size: int) -> bool:
+    def is_other_ship_around(ship_coords: list, row_size: int, column_size: int, field: list) -> bool:
         """searching if any ship is close to our"""
 
         return False
 
-    def setting_ship_coords(x: int, y: int, row_size: int, column_size: int) -> List[tuple]:
+    def setting_ship_coords(x: int, y: int, row_size: int, column_size: int, field: list) -> List[tuple]:
         """
         this function is searching parts of ship
-        :param x, y, row_size, column_size:
+        :param x, y, row_size, column_size, field:
         :return list of ship coordinates:
         """
-
         ship_coords = [(x, y)]
+        ship_is_vertical = True
+        if x != row_size:
+            while is_1(field[x + 1][y]):
+                x += 1
+                ship_coords.append((x, y))
+                added_coordinates.add((x, y))
+                ship_is_vertical = False
+        if y != column_size and ship_is_vertical:
+            while is_1(field[x][y + 1]):
+                y += 1
+                ship_coords.append((x, y))
+                added_coordinates.add((x, y))
         return ship_coords
 
     for x in range(column_size):
